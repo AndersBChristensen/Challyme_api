f01d46f5d747ab5885d604b99f42cb89801553f6
Anders Butzbach Christensen <anders@chally.me>
2016-10-20 13:05:59 +0200

    Fucking it all up igen fu

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ActiveRecord::Base
 
 
 	def friend_status
-			if User.includes(:friends).where(friends: {friend_one_id: self.id, friends: {friend_two_id: doorkeeper_token.resource_owner_id}}).count > 0
+			if Friend.where(friend_one_id: self.id, friend_two_id: doorkeeper_token.resource_owner_id).count > 0
 				status = true
-			elsif User.includes(:friends).where(friends: {friend_two_id: self.id, friends: {friend_one_id: doorkeeper_token.resource_owner_id}}).count > 0
+			elsif Friend.where(friend_two_id: self.id, friend_one_id: doorkeeper_token.resource_owner_id).count > 0
 				status = true
     	else
 				status = false
 			end
-		return status
+			status
 	end
 
 
