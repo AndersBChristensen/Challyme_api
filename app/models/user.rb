39a5a8e89ee1d9be3f3a384ae05c5515cbbcb5b8
Anders Butzbach Christensen <anders@chally.me>
2016-10-25 12:13:14 +0200

    fdfd

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,21 @@ class User < ActiveRecord::Base
 
   has_attached_file :profileimage, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profileimage, content_type: /\Aimage\/.*\z/
+# Validate filename
+  validates_attachment_file_name :profileimage, :matches => [/png\Z/, /jpe?g\Z/]
+
+# Explicitly do not validate
+  do_not_validate_attachment_file_type :profileimage
+
 
   has_attached_file :coverimage, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :coverimage, content_type: /\Aimage\/.*\z/
+# Validate filename
+  validates_attachment_file_name :coverimage, :matches => [/png\Z/, /jpe?g\Z/]
+
+# Explicitly do not validate
+  do_not_validate_attachment_file_type :coverimage
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
