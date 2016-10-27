5150083c9db9ee6e229ba95fbc21c28143184820
Anders Butzbach Christensen <anders@chally.me>
2016-10-27 12:04:06 +0200

    sfsfd

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -11,12 +11,15 @@ class Friend < ActiveRecord::Base
     logger.info(dk_id)
 
     if f1_id == dk_id
+      logger.info('inside if')
       id = f2_id
       id
     elsif f2_id == dk_id
+      logger.info('elseif')
       id = f1_id
       id
     else
+      logger.info('else')
       id = 3
       id
     end
