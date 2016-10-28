a1b9621bd2947ff449413a59bb8dc7d9afd01320
Anders Butzbach Christensen <anders@chally.me>
2016-10-28 12:55:23 +0200

    added description on completes

--- app/controllers/api/challenges_controller.rb
+++ app/controllers/api/challenges_controller.rb
@@ -1,106 +1,130 @@
 class Api::ChallengesController < ApplicationController
-	#before_action :doorkeeper_authorize!
-	skip_before_action :verify_authenticity_token
-	before_action :set_challenge, only: [:show, :edit, :update, :destroy]
+  #before_action :doorkeeper_authorize!
+  skip_before_action :verify_authenticity_token
+  before_action :set_challenge, only: [:show, :edit, :update, :destroy]
 
-	def index
-		challenges = Challenge.all
-		render json:challenges
-	end
+  def index
+    challenges = Challenge.all
+    render json: challenges
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def create
+  def create
 
-		# Todo - Create challenge
+    # Todo - Create challenge
 
-		challenge = Challenge.create(challenge_params)
+    challenge = Challenge.create(challenge_params)
 
-		
 
-		if challenge
-			render :status => :created, :json => challenge
-		else
-			render :status => 400 
-		end
-	end
+    if challenge
+      render :status => :created, :json => challenge
+    else
+      render :status => 400
+    end
+  end
 
-	def update
+  def update
 
-		respond_to do |format|
-			if challenge.update(challenge_params)
-				render :status => :updated, :json => challenge
-			else
-				render :status => 400
-			end
-		end
-	end
+    respond_to do |format|
+      if challenge.update(challenge_params)
+        render :status => :updated, :json => challenge
+      else
+        render :status => 400
+      end
+    end
+  end
 
-	def destroy
-		challenge.destroy
-		respond_to do |format|
-			msg = { :status => "ok", :message => "success", :html => "<b>...</b>" }
-    		format.json  { render :json => msg }
-    	end
-	end
+  def destroy
+    challenge.destroy
+    respond_to do |format|
+      msg = {:status => "ok", :message => "success", :html => "<b>...</b>"}
+      format.json { render :json => msg }
+    end
+  end
 
-	# Forventet json:
-	# {
-	#   challenge: {
-	#  	  title: "Månedens program",
-	#     prize: "En tur i biografen"	
-	#     tasks: [
-	#       {
-	#         title: "sdflkjelr"
-	#					dates: [ "2016-01-01", "2016-02-01" ]
-	#         actions: [
-	#           {
-	#             title: "sdiljfoier"
-	#             type: "sldkfjlier"
-    #           }
-    #         ]
-    #       }
-	#     ]
-	#   },
-	#   receiver_ids: [ 12093809123, 123102983, 123098123 ]
-	# }
-	#
-	def create_with_receivers
+  # Forventet json:
+  # {
+  #   challenge: {
+  #  	  title: "Månedens program",
+  #     prize: "En tur i biografen"
+  #     tasks: [
+  #       {
+  #         title: "sdflkjelr"
+  #					dates: [ "2016-01-01", "2016-02-01" ]
+  #         actions: [
+  #           {
+  #             title: "sdiljfoier"
+  #             type: "sldkfjlier"
+  #           }
+  #         ]
+  #       }
+  #     ]
+  #   },
+  #   receiver_ids: [ 12093809123, 123102983, 123098123 ]
+  # }
+  #
+  def create_with_receivers
 
-		challenge_params = params.require(:challenge).permit(:title, :prize, :status, :user_id, tasks_attributes: [:id, :title,
-																																														actions_attributes: [:id, :name, actionmodules_attributes:[:id, :text, :countertype, :countertime]],
-																																																							 task_dates_attributes: [:id, :date]])
+    challenge_params = params.require(:challenge).permit(:title, :prize, :status, :user_id, tasks: [
+        :id, :title,
+        actions: [
+            :id, :name, actionmodule: [
+             :id, :text, :countertype, :countertime
+         ]
+        ],
+        task_dates: [:id, :date]
+    ])
 
-		receiver_ids = params.require(:receiver_ids)
+    # challenge_params[:challenge][:whatever] = 'something'
+    # puts challenge_params.to_yaml
 
-		challenge = Challenge.create(challenge_params)
+    tasks = challenge_params.delete(:tasks).map do |task|
+      task[:actions_attributes] = task.delete(:actions).map do |action|
+        if action[:actionmodule].present?
+          action[:actionmodule_attributes] = task.delete(:actionmodules)
+        end
+        action
+      end
+      task[:task_dates_attributes] = task.delete(:task_dates)
+      task
+    end
+    challenge_params[:tasks_attributes] = tasks
 
-		unless challenge
-			render status: :bad_request
-			return
-		end
 
-		receiver_ids.each do |user_id|
-			if challenge.user_id == user_id
+    challenge_params.permit!
+
+    receiver_ids = params.require(:receiver_ids)
+
+    challenge = Challenge.create(challenge_params)
+
+    unless challenge
+      render status: :bad_request
+      return
+    end
+
+    receiver_ids.each do |user_id|
+      if challenge.user_id == user_id
         Invite.create(challenge_id: challenge.id, user_id: user_id, accepted: true)
       else
         Invite.create(challenge_id: challenge.id, user_id: user_id)
       end
-		end
+    end
 
 
-		render status: :created, json: challenge
-	end
+    render status: :created, json: challenge
+  end
 
-	private
-		def set_challenge
-			challenge = Challenge.find(params[:id])
-			render json: challenge
-		end
-		def challenge_params
-      		params.require(:challenge).permit(:title, :prize)
-    	end
+  private
+  def set_challenge
+    challenge = Challenge.find(params[:id])
+    render json: challenge
+  end
 
- 
+  def challenge_params
+    params.require(:challenge).permit(:title, :prize)
+  end
+
+
 end
