279111d37c916b4139c81bb6aa9c319d1abb4f06
Anders Butzbach Christensen <anders@chally.me>
2016-11-15 10:21:53 +0100

    giving point when completing

--- app/models/user_point.rb
+++ app/models/user_point.rb
@@ -2,7 +2,7 @@ class UserPoint < ActiveRecord::Base
 
   belongs_to :user
 
-  def give_point(point_type, user_id)
+  def self.give_point(user_id, point_type)
 
     add_point = UserPoint.create({user_id: user_id, point_type: point_type})
 
