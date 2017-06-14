71fa90724fc8b6a4201375504b27534c2965acd1
Anders Butzbach Christensen <anders@chally.me>
2017-06-14 21:13:26 +0200

    Navigationbar

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,2 +1,22 @@
 class BackendController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def numberOfUsers
+    @users = User.count
+
+  end
+
 end
